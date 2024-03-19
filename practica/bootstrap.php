c8474888ed3b7d3d2bb2a24c8859954fb10018fe
orlandot23 <164089562+orlandot23@users.noreply.github.com>
2024-03-19 15:26:31 -0600

    Add files via upload

---
+++ practica/bootstrap.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <meta name="keywords"=content=" ">
+        <meta name="description"=content=" ">
+        <link real="styleshet"href="css/bootstrap.min.css">
+        <title>Registro</title>
+    </head>
+    <body>
+        <!--navbar-->
+        <?php include="include/navbar.php";?>
+        <?php include="include/reg.php";?>
+        <?php include="include/footer.php";?>
+        <script src="js.bootstrap.min.js"></script>
+    </body>
+</html>
